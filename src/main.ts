4ada457929e5d92f452b3b05bd3027c58a189e8e
Ignacio Iglesias Campoy <iiglesiascampoy@gmail.com>
2021-09-03 02:28:55 -0600

    Fix cors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
